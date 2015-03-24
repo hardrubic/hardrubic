db044e0bb3efdf2e4ff0e3414a344643c984363d
hardrubic <dgzx106@163.com>
2015-03-24 11:25:45 +0800

    调整项目目录

--- hardrubic/blog/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from hardrubic.blog.models import BlogArticle
-
-admin.site.register(BlogArticle);
-
--- hardrubic/blog/blog_admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from hardrubic.blog.models import BlogArticle,BlogArticleAdmin
-
-admin.site.register(BlogArticle, BlogArticleAdmin)
